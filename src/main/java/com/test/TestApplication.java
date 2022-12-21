6a5f830c91f833723b14a808b14e315c0a0eb36e
dpanigrahy2020 <66458175+dpanigrahy2020@users.noreply.github.com>
2022-12-21 14:46:27 +0530

    Update TestApplication.java

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -16,7 +16,7 @@ public class TestApplication {
 	@RequestMapping("/")
 	public String message()
 	{
-		return "Welcome Friends..Happy Learning to Jenkins CI/CD pipeline target EKS.";
+		return "Welcome Friends..";
 	}
 
 }
